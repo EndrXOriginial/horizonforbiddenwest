09cbd2b670efc13717fb9ed90899cc4dc65002c2
EndrXOriginial <chris.pin@hotmail.ca>
2024-06-18 12:56:56 -0400

    adjusted summary for tablet

--- src/Components/Summary/Summary.js
+++ src/Components/Summary/Summary.js
@@ -8,7 +8,7 @@ import aloyHollywood from '../../images/aloy-hollywood.png';
 
 const Summary = () => {
     return (
-        <section className="summary">
+        <section className="summary" id='summary'>
             <svg className='static-border-summary' id="visual" viewBox="0 0 1980 150">
                 <path d="M0 31L86 31L86 50L172 50L172 33L258 33L258 78L344 78L344 42L430 42L430 62L517 62L517 38L603 38L603 54L689 54L689 47L775 47L775 80L861 80L861 58L947 58L947 66L1033 66L1033 70L1119 70L1119 79L1205 79L1205 87L1291 87L1291 48L1377 48L1377 27L1463 27L1463 60L1550 60L1550 81L1636 81L1636 52L1722 52L1722 41L1808 41L1808 33L1894 33L1894 69L1980 69L1980 80L1980 151L1980 151L1894 151L1894 151L1808 151L1808 151L1722 151L1722 151L1636 151L1636 151L1550 151L1550 151L1463 151L1463 151L1377 151L1377 151L1291 151L1291 151L1205 151L1205 151L1119 151L1119 151L1033 151L1033 151L947 151L947 151L861 151L861 151L775 151L775 151L689 151L689 151L603 151L603 151L517 151L517 151L430 151L430 151L344 151L344 151L258 151L258 151L172 151L172 151L86 151L86 151L0 151Z" fill="#374c39"></path>
                 <path d="M0 80L86 80L86 75L172 75L172 54L258 54L258 58L344 58L344 92L430 92L430 103L517 103L517 57L603 57L603 93L689 93L689 103L775 103L775 59L861 59L861 65L947 65L947 102L1033 102L1033 105L1119 105L1119 55L1205 55L1205 89L1291 89L1291 81L1377 81L1377 90L1463 90L1463 55L1550 55L1550 84L1636 84L1636 90L1722 90L1722 75L1808 75L1808 85L1894 85L1894 86L1980 86L1980 75L1980 151L1980 151L1894 151L1894 151L1808 151L1808 151L1722 151L1722 151L1636 151L1636 151L1550 151L1550 151L1463 151L1463 151L1377 151L1377 151L1291 151L1291 151L1205 151L1205 151L1119 151L1119 151L1033 151L1033 151L947 151L947 151L861 151L861 151L775 151L775 151L689 151L689 151L603 151L603 151L517 151L517 151L430 151L430 151L344 151L344 151L258 151L258 151L172 151L172 151L86 151L86 151L0 151Z" fill="#383423"></path>
@@ -18,12 +18,12 @@ const Summary = () => {
             <div className="summary-content-wrapper">
                 <h2 className="summary-title">Welcome to the world of Horizon</h2>
                 <p className="summary-under-title">In an uncharted land, explore and discover the mysteries surrounding this wild and dangerous world filled with majestic views but the wildest beasts known to humans. New Ennemies, mysteries and a whole new area to discover and explore the way you see fit!</p>
-                <div className="card-wrapper">
-                    <Card title='Brave an expensive open world' content='Discover distant lands, new enemies, rich cultures and striking characters.' bgPic={aloyForest}/>
-                    <Card title='A Majestic Frontier' content='Explore the lush forests, sunken cities and towering mountains of a far-future America.' bgPic={aloyWall}/>
-                    <Card title='Confront New Dangers' content='Engage in strategic battles against enormous machines and mounted human enemies by using weapons, gear and traps crafted from salvaged parts.' bgPic={aloyShellsnaper}/>
-                    <Card title='Unravel Startling Mysteries' content="Uncover the secret behind Earth's imminent collapse and unlock a hidden chapter in the ancient past... one that will change Aloy forever." bgPic={aloyHollywood}/>
-                </div>
+            </div>
+            <div className="card-wrapper">
+                <Card title='Brave an expensive open world' content='Discover distant lands, new enemies, rich cultures and striking characters.' bgPic={aloyForest}/>
+                <Card title='A Majestic Frontier' content='Explore the lush forests, sunken cities and towering mountains of a far-future America.' bgPic={aloyWall}/>
+                <Card title='Confront New Dangers' content='Engage in strategic battles against enormous machines and mounted human enemies by using weapons, gear and traps crafted from salvaged parts.' bgPic={aloyShellsnaper}/>
+                <Card title='Unravel Startling Mysteries' content="Uncover the secret behind Earth's imminent collapse and unlock a hidden chapter in the ancient past... one that will change Aloy forever." bgPic={aloyHollywood}/>
             </div>
         </section>
     )
